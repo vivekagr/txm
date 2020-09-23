b7ced447ff179b11cd44951b1219603b582d9022
Vivek Agarwal <me@vivek.im>
2020-09-23 17:28:02 +0530

    Add tailwind css.

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,13 +1,15 @@
-<script type='module'>
+<script>
 	import { setClient } from 'svelte-apollo';
-	import Router from 'svelte-spa-router'
+    import Router from 'svelte-spa-router';
+    import TailwindCss from './TailwindCss.svelte';
 
-	import routes from './routes/index'
+	import routes from './routes/';
 	import { client } from './apollo';
 
     setClient(client);
 </script>
 
+<TailwindCss/>
 <body>
 	<Router {routes} />
 </body>
