3d86269d8d8875d28c86081c3ecdca1f601c7038
Vivek Agarwal <me@vivek.im>
2020-09-26 18:03:24 +0530

    Add account detail page.

---
+++ app/src/routes/Accounts/AccountDetail.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { getClient, query } from 'svelte-apollo';
+  import QUERIES from '../../queries';
+  import TransactionList from '../Transactions/_TransactionList.svelte';
+  import AccountForm from './AccountForm.svelte';
+
+  export let params = {};
+
+  let searchQuery = null;
+
+  const client = getClient();
+  $: transactions = query(client, {
+    query: QUERIES.TRANSACTION.SEARCH,
+    variables: {
+      accountId: parseInt(params.id),
+      searchQuery: searchQuery || null
+    }
+  });
+
+  let account;
+  let accounts = query(client, { query: QUERIES.ACCOUNTS.ALL });
+  // try to extract account object if accountId param is available
+  $: accounts.result().then(r => {
+    const _accounts = r.data.accounts.nodes.filter(a => a.id === parseInt(params.id));
+    console.log('accounts - ', _accounts, r);
+    account = _accounts.length ? _accounts[0] : null;
+  })
+
+  let formVisible = false;
+  function toggleForm() {
+    formVisible = !formVisible;
+  }
+</script>
+
+<div>
+  <h1 class='inline-block'>
+    <a class='heading' href='#/accounts'>Accounts</a>
+  </h1>
+  {#if account}
+  <span class='inline-block mx-1'>&gt;</span>
+  <span class='font-bold text-green-700'>{account.bank} –– {account.number}</span>
+  <button class="btn-plain btn-plain-blue ml-2 float-right" on:click={toggleForm} disabled={formVisible}>Edit Account</button>
+  {/if}
+  <input type='text' placeholder='abc' bind:value={searchQuery} />
+</div>
+
+{#if formVisible}
+<AccountForm account={account} cancelCallback={toggleForm} />
+{/if}
+
+{#await $transactions then response}
+  <TransactionList transactions={response.data.transactionsSearch.nodes} />
+{/await}
--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -1,12 +1,13 @@
 import Accounts from './Accounts.svelte';
+import AccountDetail from './Accounts/AccountDetail.svelte';
 import TransactionImports from './Transactions/TransactionImports.svelte';
 import TransactionImportDetail from './Transactions/TransactionImportDetail.svelte';
 import TransactionImportNew from './Transactions/TransactionImportNew.svelte';
 
 const routes = {
   '/': Accounts,
-  '/accounts/:accountId?': Accounts,
-  // '/transactions/:accountId?': ImportTransactions
+  '/accounts': Accounts,
+  '/accounts/:id': AccountDetail,
   '/transactions/imports/': TransactionImports,
   '/transactions/imports/new': TransactionImportNew,
   '/transactions/imports/:id': TransactionImportDetail,
