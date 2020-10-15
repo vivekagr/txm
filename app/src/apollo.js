ad72aceaca1965db10485f8c0a86f0016e27a003
Vivek Agarwal <me@vivek.im>
2020-10-15 19:43:01 +0530

    Configure apollo client instance to read jwt from localStorage.

--- app/src/apollo.js
+++ app/src/apollo.js
@@ -1,7 +1,23 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5433/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:5433/graphql',
-  cache: new InMemoryCache()
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
