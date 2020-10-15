978a4e749db15d4866a106880c7de0bb85586246
Vivek Agarwal <me@vivek.im>
2020-10-15 22:08:18 +0530

    GraphQL optimisation - configure field merge to preserve data & cache redirect for Account type.

--- app/src/apollo.js
+++ app/src/apollo.js
@@ -19,5 +19,29 @@ const authLink = setContext((_, { headers }) => {
 
 export const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      TransactionImport: {
+        fields: {
+          // when moving from TransactionImport detail page to list page, preserve the
+          // transactions data (loaded on list page) by merging it
+          transactions: {
+            merge: true,
+          }
+        }
+      },
+      Query: {
+        fields: {
+          // pick out account object from cache that might have been there
+          // when account list was loaded
+          account(_, { args, toReference }) {
+            return toReference({
+              __typename: 'Account',
+              id: args.id
+            })
+          }
+        }
+      }
+    }
+  }),
 });
