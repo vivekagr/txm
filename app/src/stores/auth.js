365018ff1cc7b3ce74b228c9d70dca335dfb0189
Vivek Agarwal <me@vivek.im>
2020-10-16 18:01:10 +0530

    Add svelte store backed by localStorage and a custom store for JWT.

---
+++ app/src/stores/auth.js
@@ -0,0 +1,35 @@
+/**
+ * Auth token store.
+ */
+
+import localStorageWritable from './localStorage'
+
+// jwt store wrapping local storage store with method for checking
+// validity of the token based on the expiry value on it
+function jwtStore() {
+  const store = localStorageWritable('authJwt')
+
+  return {
+    ...store,
+    isValid() {
+      try {
+        const token = this.get()
+        if (!token) return false
+
+        const { exp } = JSON.parse(atob(token.split('.')[1]))
+        if (exp < Date.now()/1000) {
+          // Reset store before returning false
+          this.reset()
+          return false
+        } else {
+          return true
+        }
+      } catch (e) {
+        return false
+      }
+    }
+  }
+}
+
+const authToken = jwtStore()
+export default authToken
---
+++ app/src/stores/localStorage.js
@@ -0,0 +1,37 @@
+/**
+ * Svelte store backed by localStorage.
+ *
+ * Inspired by - https://gist.github.com/joshnuss/aa3539daf7ca412202b4c10d543bc077
+ */
+
+import { writable as baseWritable } from 'svelte/store'
+
+export default function writable(key, defaultValue = null) {
+  // Initialise writable store with given default value
+  const store = baseWritable(defaultValue)
+
+  // Check existing value in localStorage
+  const existingValue = localStorage.getItem(key)
+  if (existingValue)
+    store.set(JSON.parse(existingValue))
+  else
+    localStorage.setItem(key, JSON.stringify(defaultValue))
+
+  return {
+    set(value) {
+      localStorage.setItem(key, JSON.stringify(value));
+      store.set(value);
+    },
+    get() {
+      let value = localStorage.getItem(key)
+      return value ? JSON.parse(value) : defaultValue;
+    },
+    reset() {
+      this.set(defaultValue);
+    },
+    update(cb) {
+      this.set(cb(this.get()));
+    },
+    subscribe: store.subscribe
+  }
+}
