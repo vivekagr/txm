1dfd7d60c42327151f6fc96e66e7363699a0e4cc
Vivek Agarwal <me@vivek.im>
2020-10-31 20:20:02 +0530

    Refactor utils functions to use array iterations instead of loops; prettier & estlint fixes.

--- app/src/routes/Transactions/utils.js
+++ app/src/routes/Transactions/utils.js
@@ -1,82 +1,76 @@
-import XLSX from 'xlsx';
-import { casual } from 'chrono-node';
+import XLSX from 'xlsx'
+import { casual } from 'chrono-node'
 
-
-export function extractTransactionsFromSheet (sheet) {
-  let sheetRange = XLSX.utils.decode_range(sheet['!ref']);
+export function extractTransactionsFromSheet(sheet) {
+  const sheetRange = XLSX.utils.decode_range(sheet['!ref'])
 
   const header = {
     start: { encoded: null, decoded: null },
-    end: { encoded: null, decoded: null }
+    end: { encoded: null, decoded: null },
   }
 
-  // find the beginning of the transactions by looking up date header cell
-  for (let cell in sheet) {
-    if (!sheet.hasOwnProperty(cell) || !sheet[cell]) continue;
+  const headerCell = Object.entries(sheet).find(([cellIndex, cell]) => {
+    const cellValue = cell.w || cell.v
+    return cellValue && ['date', 'txn date'].indexOf(cellValue.toString().trim().toLowerCase()) > -1
+  })
 
-    let cellValue = sheet[cell].w || sheet[cell].v;
-    if (!cellValue) continue;
-
-    if (['date', 'txn date'].indexOf(cellValue.toString().trim().toLowerCase()) > -1) {
-      header.start.encoded = cell;
-      header.start.decoded = XLSX.utils.decode_cell(cell);
-      break
-    }
+  if (!headerCell) {
+    throw Error("Coudln't find the date header cell")
   }
 
-  if (!header.start.encoded) {
-    throw Error('Coudln\'t find the date header cell');
-  }
+  const [headerCellIndex] = headerCell
+  header.start.encoded = headerCellIndex
+  header.start.decoded = XLSX.utils.decode_cell(headerCellIndex)
 
   // calculate the end cell on header row naively by just looking up sheet's last column
   header.end.decoded = {
     r: header.start.decoded.r,
-    c: sheetRange.e.c
-  };
-  header.end.encoded = XLSX.utils.encode_cell(header.end.decoded);
+    c: sheetRange.e.c,
+  }
+  header.end.encoded = XLSX.utils.encode_cell(header.end.decoded)
 
   // if the sheet's last column isn't valid for the transaction header row,
   // then iterate through the entire row to find the last valid header cell
   if (!sheet[header.end.encoded]) {
-    for (let i = sheetRange.s.c; i <= sheetRange.e.c; i++) {
-      const _cell = XLSX.utils.encode_cell({ c: i, r: header.start.decoded.r });
+    for (let i = sheetRange.s.c; i <= sheetRange.e.c; i += 1) {
+      const cell = XLSX.utils.encode_cell({ c: i, r: header.start.decoded.r })
       // if present cell is empty, set previous cell as the last header row cell
-      if (!sheet[_cell]) {
-        header.end.decoded = { c: i - 1, r: header.start.decoded.r };
-        header.end.encoded = XLSX.utils.encode_cell(header.end.decoded);
-        break;
+      if (!sheet[cell]) {
+        header.end.decoded = { c: i - 1, r: header.start.decoded.r }
+        header.end.encoded = XLSX.utils.encode_cell(header.end.decoded)
+        break
       }
     }
   }
 
   // Assume that the last row for the entire sheet is the last transaction row
-  let lastRowIndex = sheetRange.e.r;
+  let lastRowIndex = sheetRange.e.r
 
   // Vertically scan down from the header beginning cell to find the last transaction row index
-  for (let r = header.start.decoded.r; r <= sheetRange.e.r; r++) {
+  for (let { r } = header.start.decoded; r <= sheetRange.e.r; r += 1) {
     // if the cell being scanned is empty, then the previous row was the last one
-    if (!sheet[XLSX.utils.encode_cell({ r: r, c: header.start.decoded.c })]) {
-      lastRowIndex = r - 1;
-      break;
+    if (!sheet[XLSX.utils.encode_cell({ r, c: header.start.decoded.c })]) {
+      lastRowIndex = r - 1
+      break
     }
   }
 
   // Compute transactions cell range and return json
   const transactionsRange = XLSX.utils.encode_range({
     s: header.start.decoded,
-    e: { r: lastRowIndex, c: header.end.decoded.c }
-  });
+    e: { r: lastRowIndex, c: header.end.decoded.c },
+  })
 
   // Build array of header cell strings
-  const headerRow = [];
-  for (let c = header.start.decoded.c; c <= header.end.decoded.c; c++) {
-    headerRow.push(sheet[XLSX.utils.encode_cell({ c: c, r: header.start.decoded.r })]);
+  const headerRow = []
+  for (let { c } = header.start.decoded; c <= header.end.decoded.c; c += 1) {
+    headerRow.push(sheet[XLSX.utils.encode_cell({ c, r: header.start.decoded.r })])
   }
 
   return {
     data: XLSX.utils.sheet_to_json(sheet, { range: transactionsRange, raw: false }),
-    headers: headerRow.map(h => h.w || h.v)
-  };
+    headers: headerRow.map((h) => h.w || h.v),
+  }
 }
 
 const HEADER_MAP = {
@@ -84,66 +78,65 @@ const HEADER_MAP = {
   narration: ['narration', 'description'],
   reference: ['chq./ref.no.', 'ref no./cheque no.'],
   debit: ['withdrawal amt.', 'debit'],
-  credit: ['deposit amt.', 'credit']
+  credit: ['deposit amt.', 'credit'],
 }
 
 // Builds <sheet header value> => <standard name> map
 const createLookupMap = (headers) => {
-  const map = {};
-
-  for (const [key, allowedValues] of Object.entries(HEADER_MAP)) {
-    for (const h of headers) {
-      if (allowedValues.indexOf(h.trim().toLowerCase()) > -1) {
-        map[key] = h;
-        break;  // break out of internal loop if match is found to prevent multiple matches
-      }
+  return Object.entries(HEADER_MAP).reduce((accumulator, [standardKey, allowedKeys]) => {
+    const matchedKey = headers.find((v) => allowedKeys.indexOf(v.trim().toLowerCase()) > -1)
+    if (matchedKey) {
+      accumulator[standardKey] = matchedKey
     }
-  }
 
-  return map;
+    return accumulator
+  }, {})
 }
 
-const _clean = s => {
-  let _s = (s || '').trim();
-  if (_s === '-') return '';
-  return _s;
+const cleanData = (s) => {
+  const cleaned = (s || '').trim()
+  if (cleaned === '-') return ''
+  return cleaned
 }
 
-const _parseFloat = n => parseFloat(n.replace(/,/g, '') || 0);
+const parseFloatString = (n) => parseFloat(n.replace(/,/g, '') || 0)
 
-var customChrono = casual.clone();
+const customChrono = casual.clone()
 customChrono.parsers.unshift({
-  pattern: () => /^(\d{1,2})[\/\-](\d{1,2})[\/\-](\d{2,4})$/,
+  pattern: () => /^(\d{1,2})[/-](\d{1,2})[/-](\d{2,4})$/,
   extract: (context, match) => {
-    let year = match[3];
-    if (match[3].length == 2) {
-      const todayYear = (new Date()).getUTCFullYear();
-      const todayTwoDigitYear = todayYear % 100;
-      const todayTwoDigitCentury = parseInt(todayYear / 100);
-      const centuryToUse = parseInt(match[3]) > todayTwoDigitYear ? todayTwoDigitCentury-1 : todayTwoDigitCentury;
-      year = String(centuryToUse) + match[3];
+    let year = match[3]
+    if (match[3].length === 2) {
+      const todayYear = new Date().getUTCFullYear()
+      const todayTwoDigitYear = todayYear % 100
+      const todayTwoDigitCentury = parseInt(todayYear / 100, 10)
+      const centuryToUse =
+        parseInt(match[3], 10) > todayTwoDigitYear ? todayTwoDigitCentury - 1 : todayTwoDigitCentury
+      year = String(centuryToUse) + match[3]
     }
 
     return {
       day: match[1],
       month: match[2],
-      year: year
+      year,
     }
-  }
-});
+  },
+})
 
 // Clean & transform the given transaction objects into standard transaction object format
 export function transformTransactionList(headers, txs) {
-  const headerLookupMap = createLookupMap(headers);
+  const headerLookupMap = createLookupMap(headers)
 
-  const transformedTxs = txs.flatMap(tx => {
-    if (/^\*+$/.test(tx[headerLookupMap['date']])) return []
+  const transformedTxs = txs.flatMap((tx) => {
+    if (/^\*+$/.test(tx[headerLookupMap.date])) return []
     return {
-      date: customChrono.parseDate(_clean(tx[headerLookupMap['date']])),
-      narrationText: _clean(tx[headerLookupMap['narration']]),
-      referenceText: _clean(tx[headerLookupMap['reference']]),
-      amount: _parseFloat(_clean(tx[headerLookupMap['debit']]) || _clean(tx[headerLookupMap['credit']])),
-      isCredit: !!_clean(tx[headerLookupMap['credit']])
+      date: customChrono.parseDate(cleanData(tx[headerLookupMap.date])),
+      narrationText: cleanData(tx[headerLookupMap.narration]),
+      referenceText: cleanData(tx[headerLookupMap.reference]),
+      amount: parseFloatString(
+        cleanData(tx[headerLookupMap.debit]) || cleanData(tx[headerLookupMap.credit])
+      ),
+      isCredit: !!cleanData(tx[headerLookupMap.credit]),
     }
   })
 
