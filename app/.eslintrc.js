7d1350b544083d938250cc7767b02a308d95f98a
Vivek Agarwal <me@vivek.im>
2020-11-10 19:54:51 +0530

    Add typescript & eslint packages; remove svelte eslint plugin due to lack of ts support.

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -4,31 +4,33 @@ module.exports = {
     es2020: true,
   },
   extends: [
-    'airbnb-base',
-    'eslint:recommended',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
-    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: [
-    'svelte3',
-  ],
-  overrides: [
-    {
-      files: ['*.svelte'],
-      processor: 'svelte3/svelte3',
-      rules: {
-        'import/first': 0,
-        'import/no-duplicates': 0,
-        'import/no-mutable-exports': 0,
-        'import/prefer-default-export': 0,
-      },
-    },
-  ],
+  // plugins: [
+  //   'svelte3',
+  // ],
+  // overrides: [
+  //   {
+  //     files: ['*.svelte'],
+  //     processor: 'svelte3/svelte3',
+  //     rules: {
+  //       'import/first': 0,
+  //       'import/no-duplicates': 0,
+  //       'import/no-mutable-exports': 0,
+  //       'import/prefer-default-export': 0,
+  //     },
+  //   },
+  // ],
   rules: {
     quotes: [
       'error',
