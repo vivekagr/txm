7c7a16be64f036611f8f0c9b9659e20c46856280
Vivek Agarwal <me@vivek.im>
2020-11-13 21:58:18 +0530

    Update import in generated `ImportTransactions.ts`.

--- app/src/data/types/ImportTransactions.ts
+++ app/src/data/types/ImportTransactions.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { TransactionTypeInput } from "./globalTypes";
+import type { TransactionTypeInput } from "./globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: ImportTransactions
