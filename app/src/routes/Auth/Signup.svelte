7210e4c43af76d90016194385c131d8224531317
Vivek Agarwal <me@vivek.im>
2020-11-17 00:03:07 +0530

    Update Signup.svelte

--- app/src/routes/Auth/Signup.svelte
+++ app/src/routes/Auth/Signup.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { mutation } from 'svelte-apollo'
   import { replace } from 'svelte-spa-router'
+  import { ApolloError, isApolloError } from '@apollo/client'
 
   import type { RegisterUser, RegisterUserVariables } from 'app/data/types/RegisterUser'
   import QUERIES from 'app/queries'
 
-  const authMutation = mutation<RegisterUser, RegisterUserVariables>(QUERIES.AUTHENTICATE)
+  const authMutation = mutation<RegisterUser, RegisterUserVariables>(QUERIES.REGISTER)
 
   interface SignupFormData {
     name?: string
@@ -16,25 +17,31 @@
   const formData: SignupFormData = {}
   $: formEnabled = formData.name && formData.username && formData.password
 
-  let wrongCredentials = false
+  let error: string | null
 
   async function handleSubmit() {
-    wrongCredentials = false
+    error = null
 
     if (!formData.name || !formData.username || !formData.password) return
 
-    const res = await authMutation({
-      variables: {
-        name: formData.name,
-        username: formData.username,
-        password: formData.password,
-      },
-    })
+    try {
+      const res = await authMutation({
+        variables: {
+          name: formData.name,
+          username: formData.username,
+          password: formData.password,
+        },
+      })
 
-    if (res.data?.registerUser?.user?.id) {
-      replace('/login')
-    } else {
-      wrongCredentials = true
+      if (res.data?.registerUser?.user?.id) {
+        replace('/login')
+      } else {
+        error = 'Unable to register'
+      }
+    } catch (e) {
+      if (isApolloError(e)) {
+        console.log(e.graphQLErrors[0].errcode)
+      }
     }
   }
 </script>
@@ -54,39 +61,49 @@
     </div>
     <form class="mt-8" on:submit|preventDefault={handleSubmit}>
       <div class="rounded-md shadow-sm">
-        <div>
+        <label class="block">
+          <span class="text-sm font-medium leading-5 text-gray-700">Name</span>
           <input
-            aria-label="Username"
-            name="username"
-            type="itext"
-            required
-            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+            type="text"
+            class="form-input mt-1 block w-full"
+            placeholder="Name"
+            aria-label="Name"
+            bind:value={formData.name}
+            required />
+        </label>
+        <label class="block mt-4">
+          <span class="text-sm font-medium leading-5 text-gray-700">Username</span>
+          <input
+            type="text"
+            class="form-input mt-1 block w-full"
             placeholder="Username"
-            bind:value={formData.username} />
-        </div>
-        <div class="-mt-px">
+            aria-label="Username"
+            bind:value={formData.username}
+            required />
+        </label>
+        <label class="block mt-4">
+          <span class="text-sm font-medium leading-5 text-gray-700">Password</span>
           <input
-            aria-label="Password"
-            name="password"
             type="password"
-            required
-            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+            class="form-input mt-1 block w-full"
             placeholder="Password"
-            bind:value={formData.password} />
-        </div>
+            aria-label="Password"
+            bind:value={formData.password}
+            required />
+        </label>
       </div>
 
-      <div class="mt-6">
+      <div class="mt-8">
         <button
           type="submit"
-          class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+          class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-semibold rounded-md uppercase text-blue-500 border-blue-200 hover:text-blue-600 hover:border-blue-300 focus:outline-none focus:border-blue-500 focus:shadow-outline-blue active:border-blue-500 transition duration-150 ease-in-out"
           disabled={!formEnabled}>
-          Sign in
+          Sign up
         </button>
       </div>
 
-      {#if wrongCredentials}
-        <div class="mt-4 text-center text-red-700 font-small">Wrong credentials</div>
+      {#if error}
+        <div class="mt-4 text-center text-red-700 font-small">Unable to register</div>
       {/if}
     </form>
   </div>
